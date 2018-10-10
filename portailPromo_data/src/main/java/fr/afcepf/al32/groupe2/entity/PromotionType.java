c3c680c84b0c2484ee093c6983f38b3d8f058dce
Nicolas Gondran <nicolas.gondran@gmail.com>
2018-10-10 16:31:44 +0200

    commit

--- portailPromo_data/src/main/java/fr/afcepf/al32/groupe2/entity/PromotionType.java
+++ portailPromo_data/src/main/java/fr/afcepf/al32/groupe2/entity/PromotionType.java
@@ -3,9 +3,12 @@ package fr.afcepf.al32.groupe2.entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Inheritance(strategy=InheritanceType.JOINED)
 public abstract class PromotionType {
 
 	@Id
