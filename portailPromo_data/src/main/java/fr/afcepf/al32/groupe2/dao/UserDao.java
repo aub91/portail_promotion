920249d35b02ada62d368d5d62abb9954d8635d5
aub1 <aubin.guilhem@gmail.com>
2018-10-20 17:04:32 +0200

    user dao new method

--- portailPromo_data/src/main/java/fr/afcepf/al32/groupe2/dao/UserDao.java
+++ portailPromo_data/src/main/java/fr/afcepf/al32/groupe2/dao/UserDao.java
@@ -4,4 +4,6 @@ import fr.afcepf.al32.groupe2.entity.User;
 
 public interface UserDao {
 	User findOneById(Long id);
+	
+	User findOneByLoginAndPassword(String login, String password);
 }
--- portailPromo_data/src/main/java/fr/afcepf/al32/groupe2/dao/impl/UserDaoImpl.java
+++ portailPromo_data/src/main/java/fr/afcepf/al32/groupe2/dao/impl/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package fr.afcepf.al32.groupe2.dao.impl;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
@@ -20,6 +21,17 @@ public class UserDaoImpl implements UserDao {
 	public User findOneById(Long id) {
 		// TODO Auto-generated method stub
 		return em.find(User.class, id);
+	}
+
+	@Override
+	public User findOneByLoginAndPassword(String login, String password) {
+		// TODO Auto-generated method stub
+		try {
+			return em.createNamedQuery("User.findByLoginAndPassword", User.class).setParameter("login", login).setParameter("password", password).getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+		
 	}
 
 }
