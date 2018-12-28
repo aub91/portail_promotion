10419bfb03edcd927b00e12842023da4d6917f0f
dimi1926 <diniko1926@gmail.com>
2018-12-28 11:06:50 +0100

    modif pour la recom client

--- portailPromo_web/src/main/java/fr/afcepf/al32/groupe2/web/delegate/WsPromoTemplateDelegateImpl.java
+++ portailPromo_web/src/main/java/fr/afcepf/al32/groupe2/web/delegate/WsPromoTemplateDelegateImpl.java
@@ -1,14 +1,5 @@
 package fr.afcepf.al32.groupe2.web.delegate;
 
-import fr.afcepf.al32.groupe2.ws.dto.CategoryProductDto;
-import fr.afcepf.al32.groupe2.ws.dto.OrchestratorResearchDtoResponse;
-import fr.afcepf.al32.groupe2.ws.itf.IWsPromoTemplate;
-import fr.afcepf.al32.groupe2.ws.wsPromoTemplate.dto.PromotionTemplateResultDto;
-import fr.afcepf.al32.groupe2.ws.wsPromoTemplate.dto.TopPromotionTemplateResultDto;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -16,49 +7,76 @@ import java.util.List;
 import java.util.Properties;
 import java.util.StringJoiner;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import fr.afcepf.al32.groupe2.ws.itf.IWsPromoTemplate;
+import fr.afcepf.al32.groupe2.ws.wsPromoTemplate.dto.TopPromotionTemplateResultDto;
+
 @Component
 public class WsPromoTemplateDelegateImpl implements IWsPromoTemplate {
 
-    private RestTemplate restTemplate;
-    private String base_url;
+	private RestTemplate restTemplate;
+	private String base_url;
+	private String base_url_client;
 
-    public WsPromoTemplateDelegateImpl() {
-        restTemplate = new RestTemplate();
-        try {
-            Properties props = new Properties();
-            InputStream inputStream = Thread.currentThread().getContextClassLoader()
-                    .getResourceAsStream("web_services.properties");
-            props.load(inputStream);
-            inputStream.close();
+	public WsPromoTemplateDelegateImpl() {
+		restTemplate = new RestTemplate();
+		try {
+			Properties props = new Properties();
+			InputStream inputStream = Thread.currentThread().getContextClassLoader()
+					.getResourceAsStream("web_services.properties");
+			props.load(inputStream);
+			inputStream.close();
 
-            this.base_url = props.getProperty("ws_promo_template.base_url");
+			this.base_url = props.getProperty("ws_promo_template.base_url");
+			this.base_url_client = props.getProperty("ws_promo_recom_client.base_url");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public TopPromotionTemplateResultDto searchTopTemplatePromoForShopKeeper(Double sourceLong, Double sourceLat, List<String> categories) {
-        StringJoiner categoriesJoiner = new StringJoiner(",");
-        categories.forEach(word -> {
-            try {
-                categoriesJoiner.add(URLEncoder.encode(word, "UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-        });
-        String url = String.format("%s/ByShopper?sourceLong=%s&sourceLat=%s&categories=%s", base_url, sourceLong , sourceLat, categoriesJoiner.toString());
+	@Override
+	public TopPromotionTemplateResultDto searchTopTemplatePromoForShopKeeper(Double sourceLong, Double sourceLat,
+			List<String> categories) {
+		StringJoiner categoriesJoiner = new StringJoiner(",");
+		categories.forEach(word -> {
+			try {
+				categoriesJoiner.add(URLEncoder.encode(word, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		});
+		String url = String.format("%s/ByShopper?sourceLong=%s&sourceLat=%s&categories=%s", base_url, sourceLong,
+				sourceLat, categoriesJoiner.toString());
 
-        TopPromotionTemplateResultDto result = null;
+		TopPromotionTemplateResultDto result = null;
 
-        try {
-            result = restTemplate.getForObject(url, TopPromotionTemplateResultDto.class);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+		try {
+			result = restTemplate.getForObject(url, TopPromotionTemplateResultDto.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        return result;
-    }
+		return result;
+	}
+
+	@Override
+	public TopPromotionTemplateResultDto searchByClientsFavoriteCategory(Double sourceLong, Double sourceLat,
+			String category) {
+
+		String url = String.format("%s/ByClient?sourceLong=%s&sourceLat=%s&categories=%s", base_url_client, sourceLong,
+				sourceLat, category);
+
+		TopPromotionTemplateResultDto result = null;
+
+		try {
+			result = restTemplate.getForObject(url, TopPromotionTemplateResultDto.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
 }
