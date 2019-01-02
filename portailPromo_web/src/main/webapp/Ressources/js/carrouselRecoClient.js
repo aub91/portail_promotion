cbcdbbd0eed7022df2b1e669033b9d1212d17a66
Ramya CANABADY <jeyacumar.ramya@gmail.com>
2019-01-02 20:03:15 +0100

    dernier changement avant de quitter l'afcepf  js et css non fonctionnel

--- portailPromo_web/src/main/webapp/Ressources/js/carrouselRecoClient.js
+++ portailPromo_web/src/main/webapp/Ressources/js/carrouselRecoClient.js
@@ -1,10 +1,26 @@
-document.getElementById("carousselRecoClient").onmouseover = function() {mouseOver()};
-document.getElementById("carousselRecoClient").onmouseout = function() {mouseOut()};
+document.getElementById("carousselRecoClientSlides").onmouseover = function() {mouseOver()};
+document.getElementById("carousselRecoClientSlides").onmouseout = function() {mouseOut()};
+document.getElementById("leftArrow").onmouseover = function() {arrowLeft()};
+document.getElementById("rightArrow").onmouseover = function() {arrowRight()};
+
 
 function mouseOver() {
-  document.getElementById("carousselRecoClient").className = "slidesOff";
+  document.getElementById("carousselRecoClientSlides").className = "slidesOff w3-col s10 l10";
 }
 
 function mouseOut() {
-  document.getElementById("carousselRecoClient").className = "slides";
+  document.getElementById("carousselRecoClientSlides").className = "slidesRight w3-col s10 l10";
 }
+
+function arrowRight(event) {
+  event.stopPropagation();
+  document.getElementById("carousselRecoClientSlides").className = "slidesRight w3-col s10 l10";
+  document.getElementById("carousselRecoClientSlides").className = "FichePromotion slideRight w3-mobile w3-row w3-col s1 l1 ";
+}
+
+function arrowLeft(event) {
+  event.stopPropagation();
+  document.getElementById("carousselRecoClientSlides").className = "slidesLeft w3-col s10 l10";
+  document.getElementById("carousselRecoClientSlides").className = "FichePromotion slideLeft w3-mobile w3-row w3-col s1 l1 ";
+}
+
